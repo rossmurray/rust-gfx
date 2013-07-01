9c1d7a79772b191eb081192c46749a9752ad0621
Ross <rm@egoorb.com>
2013-07-01 04:17:16 -0500

    adds imports for many of the functions in SDL_gfxPrimitives.h

--- src/gfx.rs
+++ src/gfx.rs
@@ -1,4 +1,6 @@
-use std::libc::{uint8_t, int16_t, int32_t};
+extern mod sdl;
+
+use std::libc::{c_int, c_schar, uint8_t, int16_t, uint32_t};
 
 use sdl::video::ll::SDL_Surface;
 use sdl::video::Surface;
@@ -8,14 +10,138 @@ use sdl::video::RGBA;
 
 #[link_args="-lSDL_gfx"]
 extern {
-	fn rectangleRGBA(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t,
-		r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> int32_t;
+	// fn rectangleRGBA(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t,
+	// 	r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
 
-	fn boxRGBA(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t,
-		r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> int32_t;
+	// fn boxRGBA(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t,
+	// 	r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
 
-	fn filledCircleRGBA(dst: *SDL_Surface, x: int16_t, y: int16_t, rad: int16_t,
-		r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> int32_t;
+	// fn filledCircleRGBA(dst: *SDL_Surface, x: int16_t, y: int16_t, rad: int16_t,
+	// 	r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn pixelColor(dst: *SDL_Surface, x: int16_t, y: int16_t, color: uint32_t) -> c_int;
+
+	fn pixelRGBA(dst: *SDL_Surface, x: int16_t, y: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn hlineColor(dst: *SDL_Surface, x1: int16_t, x2: int16_t, y: int16_t, color: uint32_t) -> c_int;
+
+	fn hlineRGBA(dst: *SDL_Surface, x1: int16_t, x2: int16_t, y: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn vlineColor(dst: *SDL_Surface, x: int16_t, y1: int16_t, y2: int16_t, color: uint32_t) -> c_int;
+
+	fn vlineRGBA(dst: *SDL_Surface, x: int16_t, y1: int16_t, y2: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn rectangleColor(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, color: uint32_t) -> c_int;
+
+	fn rectangleRGBA(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn roundedRectangleColor(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, rad: int16_t, color: uint32_t) -> c_int;
+
+	fn roundedRectangleRGBA(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, rad: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn boxColor(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, color: uint32_t) -> c_int;
+
+	fn boxRGBA(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn roundedBoxColor(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, rad: int16_t, color: uint32_t) -> c_int;
+
+	fn roundedBoxRGBA(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, rad: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn lineColor(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, color: uint32_t) -> c_int;
+
+	fn lineRGBA(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn aalineColor(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, color: uint32_t) -> c_int;
+
+	fn aalineRGBA(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn thickLineColor(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, width: uint8_t, color: uint32_t) -> c_int;
+
+	fn thickLineRGBA(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, width: uint8_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn circleColor(dst: *SDL_Surface, x: int16_t, y: int16_t, rad: int16_t, color: uint32_t) -> c_int;
+
+	fn circleRGBA(dst: *SDL_Surface, x: int16_t, y: int16_t, rad: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn arcColor(dst: *SDL_Surface, x: int16_t, y: int16_t, rad: int16_t, start: int16_t, end: int16_t, color: uint32_t) -> c_int;
+
+	fn arcRGBA(dst: *SDL_Surface, x: int16_t, y: int16_t, rad: int16_t, start: int16_t, end: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn aacircleColor(dst: *SDL_Surface, x: int16_t, y: int16_t, rad: int16_t, color: uint32_t) -> c_int;
+
+	fn aacircleRGBA(dst: *SDL_Surface, x: int16_t, y: int16_t, rad: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn filledCircleColor(dst: *SDL_Surface, x: int16_t, y: int16_t, r: int16_t, color: uint32_t) -> c_int;
+
+	fn filledCircleRGBA(dst: *SDL_Surface, x: int16_t, y: int16_t, rad: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn ellipseColor(dst: *SDL_Surface, x: int16_t, y: int16_t, rx: int16_t, ry: int16_t, color: uint32_t) -> c_int;
+
+	fn ellipseRGBA(dst: *SDL_Surface, x: int16_t, y: int16_t, rx: int16_t, ry: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn aaellipseColor(dst: *SDL_Surface, x: int16_t, y: int16_t, rx: int16_t, ry: int16_t, color: uint32_t) -> c_int;
+
+	fn aaellipseRGBA(dst: *SDL_Surface, x: int16_t, y: int16_t, rx: int16_t, ry: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn filledEllipseColor(dst: *SDL_Surface, x: int16_t, y: int16_t, rx: int16_t, ry: int16_t, color: uint32_t) -> c_int;
+
+	fn filledEllipseRGBA(dst: *SDL_Surface, x: int16_t, y: int16_t, rx: int16_t, ry: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn pieColor(dst: *SDL_Surface, x: int16_t, y: int16_t, rad: int16_t, start: int16_t, end: int16_t, color: uint32_t) -> c_int;
+
+	fn pieRGBA(dst: *SDL_Surface, x: int16_t, y: int16_t, rad: int16_t, start: int16_t, end: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn filledPieColor(dst: *SDL_Surface, x: int16_t, y: int16_t, rad: int16_t, start: int16_t, end: int16_t, color: uint32_t) -> c_int;
+
+	fn filledPieRGBA(dst: *SDL_Surface, x: int16_t, y: int16_t, rad: int16_t, start: int16_t, end: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn trigonColor(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, x3: int16_t, y3: int16_t, color: uint32_t) -> c_int;
+
+	fn trigonRGBA(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, x3: int16_t, y3: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn aatrigonColor(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, x3: int16_t, y3: int16_t, color: uint32_t) -> c_int;
+
+	fn aatrigonRGBA(dst: *SDL_Surface,  x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, x3: int16_t, y3: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	fn filledTrigonColor(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, x3: int16_t, y3: int16_t, color: uint32_t) -> c_int;
+
+	fn filledTrigonRGBA(dst: *SDL_Surface, x1: int16_t, y1: int16_t, x2: int16_t, y2: int16_t, x3: int16_t, y3: int16_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	//fn polygonColor(dst: *SDL_Surface, const int16_t * vx, const int16_t * vy, n: int32_t, color: uint32_t) -> c_int;
+
+	//fn polygonRGBA(dst: *SDL_Surface, const int16_t * vx, const int16_t * vy, n: int32_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	//fn aapolygonColor(dst: *SDL_Surface, const int16_t * vx, const int16_t * vy, n: int32_t, color: uint32_t) -> c_int;
+
+	//fn aapolygonRGBA(dst: *SDL_Surface, const int16_t * vx, const int16_t * vy, n: int32_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	//fn filledPolygonColor(dst: *SDL_Surface, const int16_t * vx, const int16_t * vy, n: int32_t, color: uint32_t) -> c_int;
+
+	//fn filledPolygonRGBA(dst: *SDL_Surface, const int16_t * vx, const int16_t * vy, n: int32_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	//fn texturedPolygon(dst: *SDL_Surface, const int16_t * vx, const int16_t * vy, n: int32_t, texture: *SDL_Surface, texture_dx: int32_t, texture_dy: int32_t) -> c_int;
+
+	//fn filledPolygonColorMT(dst: *SDL_Surface, const int16_t * vx, const int16_t * vy, n: int32_t, color: uint32_t, int **polyInts, int *polyAllocated) -> c_int;
+
+	//fn filledPolygonRGBAMT(dst: *SDL_Surface, const int16_t * vx, const int16_t * vy, n: int32_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t, int **polyInts, int *polyAllocated) -> c_int;
+
+	//fn texturedPolygonMT(dst: *SDL_Surface, const int16_t * vx, const int16_t * vy, n: int32_t, texture: *SDL_Surface, texture_dx: int32_t, texture_dy: int32_t, int **polyInts, int *polyAllocated) -> c_int;
+
+	//fn bezierColor(dst: *SDL_Surface, const int16_t * vx, const int16_t * vy, n: int32_t, s: int32_t, color: uint32_t) -> c_int;
+
+	//fn bezierRGBA(dst: *SDL_Surface, const int16_t * vx, const int16_t * vy, n: int32_t, s: int32_t, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	//fn gfxPrimitivesSetFont(const void *fontdata, cw: uint32_t, ch: uint32_t);
+
+	fn gfxPrimitivesSetFontRotation(rotation: uint32_t);
+
+	fn characterColor(dst: *SDL_Surface, x: int16_t, y: int16_t, c: c_schar, color: uint32_t) -> c_int;
+
+	fn characterRGBA(dst: *SDL_Surface, x: int16_t, y: int16_t, c: c_schar, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
+
+	//fn stringColor(dst: *SDL_Surface, x: int16_t, y: int16_t, const char *s, color: uint32_t) -> c_int;
+
+	//fn stringRGBA(dst: *SDL_Surface, x: int16_t, y: int16_t, const char *s, r: uint8_t, g: uint8_t, b: uint8_t, a: uint8_t) -> c_int;
 }
 
 pub fn rectangle_rgba(screen: &Surface, x1: i16, y1: i16, x2: i16, y2: i16, r: u8, g: u8, b: u8, a: u8) {
